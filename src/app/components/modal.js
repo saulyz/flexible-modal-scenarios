ebaa3d7ab05d00827d09d56d06a7e64f667ace3e
Saulius Vikerta <saulius.vikerta@nfq.lt>
2018-05-08 21:26:05 +0300

    Modal class

--- src/app/components/modal.js
+++ src/app/components/modal.js
@@ -1,13 +1,54 @@
+import modalService from './modal-service';
+
 export class Modal {
     constructor() {
+        this.component = null;
         this.title = '';
         this.customClass = '';
-        this.transition = 'fade-zoom';
-        this.component = null;
         this.parameters = null;
+        this.transition = 'fade-zoom';
         this.resolver = null;
         this.handler = new Promise(resolve => this.resolver = resolve);
     }
 
-    //...
+    setComponent(component) {
+        this.component = component;
+
+        return this;
+    }
+    
+    setTitle(title) {
+        this.title = title;
+        
+        return this;
+    }
+
+    setParameters(parameters) {
+        this.parameters = parameters;
+
+        return this;
+    }
+
+    setCustomClass(classList) {
+        this.customClass = classList;
+
+        return this;
+    }
+
+    setTransition(transition) {
+        this.transition = transition;
+
+        return this;
+    }
+
+    open() {
+        this.modalService.open(this);
+
+        return this.handler;
+    }
+
+    onClose(answer) {
+        this.resolver(answer);
+    }
+
 }
