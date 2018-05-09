65061c960e0e4ef485a6205331a8ed9726c2dfb2
Saulius Vikerta <saulius.vikerta@nfq.lt>
2018-05-09 22:09:22 +0300

    modal as view component

--- src/views/common/modal/modal.js
+++ src/views/common/modal/modal.js
@@ -1,5 +1,32 @@
+import eventBus from 'App/components/event-bus';
+
 export default {
     template: '#modal-template',
     name: 'modal',
-    props: [ 'component', 'title', 'classList', 'transition' ]
+
+    data() {
+        return {
+            modal: null,
+            isOpen: false
+        }
+    },
+
+    created() {
+        eventBus.$on('modal.open', (modal) => this.open(modal));
+        eventBus.$on('modal.close', (answer) => this.close(answer));
+    },
+
+    methods: {
+        open(modal) {
+            this.modal = modal;
+            this.isOpen = true;
+        },
+        
+        close(answer = null) {
+            this.modal.onClose(answer);
+            
+            this.modal = null;
+            this.isOpen = false;
+        }
+    }
 }
