83ea0ffc9a407697a71f41e797806332552fc9b7
Saulius Vikerta <saulius.vikerta@nfq.lt>
2018-05-13 01:05:35 +0300

    css: colors and buttons

--- src/views/modals/demo/demo.js
+++ src/views/modals/demo/demo.js
@@ -1,4 +1,10 @@
 export default {
     name: 'demo-modal',
-    template: `<div>This is demo modal component that has no functionality.</div>`
+    template: `
+        <div>
+            <p class="mb-30">This is demo modal component that has no functionality.</p>
+            <button class="btn btn-medium btn-blue">Yes button</button>
+            <button class="btn btn-medium">No button</button>
+        </div>
+    `
 }
