c644cf6ac1e53fc2e08016543cd3a1d9c0a2c742
Saulius Vikerta <saulius.vikerta@nfq.lt>
2018-05-14 00:08:00 +0300

    dumb and api based modal components

---
+++ src/views/modals/api-luck/api-luck.js
@@ -0,0 +1,59 @@
+import actionService from 'App/services/action-service';
+
+import { faThumbsUp } from '@fortawesome/fontawesome-free-regular';
+import { faThumbsDown } from '@fortawesome/fontawesome-free-regular';
+
+export default {
+    name: 'api-luck',
+    template: `
+        <div>
+            <p class="mb-30">This is luck checker API test. On component create ir requests API and shows the API response.</p>
+            <div class="api-response-wrapper">
+                <div v-if="response" class="response-icon-wrapper">    
+                    <div class="response-icon-box"
+                        :class="{'success': success }">
+                        <font-awesome-icon 
+                            class="response-icon"
+                            :class="{'success': success }" 
+                            :icon="icon">
+                        </font-awesome-icon>
+                    </div>
+                </div>
+                <pre v-text="response"></pre>
+            </div>
+            <div class="text-align-center">
+                <button class="btn btn-medium" @click="onClose('user close')">Close</button>
+            </div>
+        </div>
+    `,
+
+    props: ['onClose'],
+
+    data() {
+        return {
+            response: null
+        }
+    },
+
+    created() {
+        actionService.testLuck()
+            .then(response => response.data)
+            .then(response => {
+                this.response = response
+            })
+    },
+
+    computed: {
+        success() {
+            return (this.response && this.response.status === 200);
+        },
+
+        icon() {
+            if (this.success) {
+                return faThumbsUp;
+            } else {
+                return faThumbsDown;
+            }
+        }
+    }
+}
--- src/views/pages/home/home.js
+++ src/views/pages/home/home.js
@@ -1,16 +1,25 @@
 import Vue from 'vue';
+
 import modalService from 'App/components/modal-service';
-import demoModal from 'Modals/demo/demo';
+import plainTextModal from 'Modals/plain-text/plain-text';
+import apiLuckModal from 'Modals/api-luck/api-luck';
 
 new Vue({
     el: '#page-home',
     name: 'page-home',
 
     methods: {
-        openModal() {
+        openModalPlainText() {
             return modalService.create()
-                .setTitle('my first modal')
-                .setComponent(demoModal)
+                .setTitle('Plaintext dumb component')
+                .setComponent(plainTextModal)
+                .open();
+        },
+
+        openModalApiLuck() {
+            return modalService.create()
+                .setTitle('Test your luck API')
+                .setComponent(apiLuckModal)
                 .open();
         }
     }
